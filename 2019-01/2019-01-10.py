ef5e7b8df81d00a209f36c8b585c3815ad11229f
zhangsan <17081371097@163.com>
2019-01-12 20:36:06 +0800

    add page num

--- 2019-01/2019-01-10.py
+++ 2019-01/2019-01-10.py
@@ -17,7 +17,6 @@ driver.find_element_by_css_selector('label[for="radio03"]').click()
 driver.find_element_by_link_text('搜索微博').click()
 
 
-eles = driver.find_elements_by_css_selector('div[action-type="feed_list_item"]')
 
 
 # todo 找到每个微博中的 标题，发送人，发布时间，来源，收藏数，转发数，评论数，点赞数
@@ -33,32 +32,40 @@ wt.write(0,5,'转发数')
 wt.write(0,6,'评论数')
 wt.write(0,7,'点赞数')
 
+
 couter = 0
-for ele in eles:
-    couter+=1
-    title = ele.find_element_by_css_selector('p[class="txt"]').text
-    username = ele.find_element_by_css_selector('a[class="name"]').text
-    time = ele.find_element_by_css_selector('p[class="from"]>a:nth-child(1)').text
-    source = ele.find_element_by_css_selector('p[class="from"]>a:nth-child(1)').text
-    coll = ele.find_element_by_css_selector('div[class="card-act"]>ul>li:nth-child(1)').text
-    coll_num = coll.split('收藏')[1]
-    forward = ele.find_element_by_css_selector('div[class="card-act"]>ul>li:nth-child(2)').text
-    forward_num = forward.split('转发')[1]
-    comment = ele.find_element_by_css_selector('div[class="card-act"]>ul>li:nth-child(3)').text
-    comment_num = comment.split('评论')[1]
-    like = ele.find_element_by_css_selector('div[class="card-act"]>ul>li:nth-child(4)').text
-    print(title,username,time,coll_num,forward_num,coll_num,like)
-    wt.write(couter,0,title)
-    wt.write(couter,1,username)
-    wt.write(couter,2,time)
-    wt.write(couter,3,source)
-    wt.write(couter,4,coll_num)
-    wt.write(couter,5,forward_num)
-    wt.write(couter,6,comment_num)
-    wt.write(couter,7,like)
+#  保存5页的数据
+for x in range(5):
+    
+    eles = driver.find_elements_by_css_selector('div[action-type="feed_list_item"]')
+    for ele in eles:
+        couter+=1
+        title = ele.find_element_by_css_selector('p[class="txt"]').text
+        username = ele.find_element_by_css_selector('a[class="name"]').text
+        time = ele.find_element_by_css_selector('p[class="from"]>a:nth-child(1)').text
+        source = ele.find_element_by_css_selector('p[class="from"]>a:nth-child(1)').text
+        coll = ele.find_element_by_css_selector('div[class="card-act"]>ul>li:nth-child(1)').text
+        coll_num = coll.split('收藏')[1]
+        forward = ele.find_element_by_css_selector('div[class="card-act"]>ul>li:nth-child(2)').text
+        forward_num = forward.split('转发')[1]
+        comment = ele.find_element_by_css_selector('div[class="card-act"]>ul>li:nth-child(3)').text
+        comment_num = comment.split('评论')[1]
+        like = ele.find_element_by_css_selector('div[class="card-act"]>ul>li:nth-child(4)').text
+        # print(title,username,time,coll_num,forward_num,coll_num,like)
+        print('counter',couter)
+        wt.write(couter,0,title)
+        wt.write(couter,1,username)
+        wt.write(couter,2,time)
+        wt.write(couter,3,source)
+        wt.write(couter,4,coll_num)
+        wt.write(couter,5,forward_num)
+        wt.write(couter,6,comment_num)
+        wt.write(couter,7,like)
+    
+    driver.find_element_by_css_selector('a[action-type="feed_list_page_more"]').click()
+    driver.find_element_by_css_selector('ul[class="s-scroll"]>li:nth-child({})'.format(x+2)).click()
 
 wb.save('weibo.xls')
-
 driver.quit()
 
 
